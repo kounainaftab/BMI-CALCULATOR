ec95c39a199d4e1c65de61194562fccf349e81b6
Kounain Aftab <ishadoll03082@gmail.com>
2025-02-05 03:15:44 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//BMI Calculator In Typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+let weight = 45;
+let height = 5.3;
+let bmi = weight / (height * height);
+console.log('konain Your BMI Is ${bmi}');
---
+++ main.ts
@@ -0,0 +1,8 @@
+//BMI Calculator In Typescript
+
+let weight = 45;
+let height =5.3;
+
+let bmi = weight/(height*height)
+
+console.log('konain Your BMI Is ${bmi}')
